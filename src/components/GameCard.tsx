fc8637c5fddf69db6c076130d04e760d33c6accd
Rajeev Hegde <rajeevhegde5@gmail.com>
2025-04-15 17:13:34 +0530

    feat: added Game details Modal ( A popup)

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IGame } from '../types/game';
-import './styles/GameCard.css'; // Importing GameCard-specific CSS
+import './styles/GameCard.css';
+import GameDetailsModal from './GameDetailsModal';
 
 const GameCard: React.FC<{ game: IGame }> = ({ game }) => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
     return (
-        <div className="card border-0 shadow-sm game-card">
-            <img src={game.background_image} className="card-img-top" alt={game.name} />
-            <div className="card-body">
-                <h5 className="card-title fw-semibold">{game.name}</h5>
-                <p className="card-text mb-1"><strong>Released:</strong> {game.released}</p>
-                <p className="card-text"><strong>Rating:</strong> {game.rating}</p>
-                <button className="btn btn-primary">Details</button>
+        <>
+            <div className="card border-0 shadow-sm game-card">
+                <img src={game.background_image} className="card-img-top" alt={game.name} />
+                <div className="card-body">
+                    <h5 className="card-title fw-semibold">{game.name}</h5>
+                    <p className="card-text mb-1"><strong>Released:</strong> {game.released}</p>
+                    <p className="card-text"><strong>Rating:</strong> {game.rating}</p>
+                    <button className="btn btn-primary" onClick={() => setIsModalOpen(true)}>Details</button>
+                </div>
             </div>
-        </div>
+
+            {isModalOpen && (
+                <GameDetailsModal game={game} closeModal={() => setIsModalOpen(false)} />
+            )}
+        </>
     );
 };
 

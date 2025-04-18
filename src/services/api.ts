da440b36b4a68cdfae77456abfd74ce0818ad820
Rajeev Hegde <rajeevhegde5@gmail.com>
2025-04-18 14:46:22 +0530

    feat: added fetchUpcomingGames API

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,3 +1,5 @@
+import { IGame } from "../types/game";
+
 const API_KEY = import.meta.env.VITE_RAWG_API_KEY;
 const BASE_URL = 'https://api.rawg.io/api';
 
@@ -6,3 +8,15 @@ export const fetchGames = async () => {
   const data = await res.json();
   return data.results;
 };
+
+export const fetchUpcomingGames = async (): Promise<IGame[]> => {
+  const today = new Date().toISOString().split('T')[0];
+  const nextYearEnd = new Date(new Date().getFullYear() + 1, 11, 31).toISOString().split('T')[0];
+
+  const res = await fetch(
+    `https://api.rawg.io/api/games?dates=${today},${nextYearEnd}&ordering=-added&page_size=20&key=${API_KEY}`
+  );
+  const data = await res.json();
+  return data.results;
+};
+

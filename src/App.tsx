940e73b0cc413373b45b71ab069bd2c0ce395de9
Rajeev Hegde <rajeevhegde5@gmail.com>
2025-04-18 14:45:35 +0530

    feat: added upcoming route

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Ad
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import SearchResults from './pages/SearchResults';
+import UpcomingGames from './pages/UpcomingGames';
 
 
 
@@ -13,6 +14,7 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/search" element={<SearchResults />} />
+        <Route path="/upcoming" element={<UpcomingGames />} />
         {/* Add routes for other pages */}
       </Routes>
     </Router>

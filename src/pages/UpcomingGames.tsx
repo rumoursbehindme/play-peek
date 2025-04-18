ce7e7b5b2a74ec693917f5876b97163d3ada4934
Rajeev Hegde <rajeevhegde5@gmail.com>
2025-04-18 14:46:06 +0530

    feat: added UpcomingGames Page

---
+++ src/pages/UpcomingGames.tsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import { IGame } from '../types/game';
+import { fetchUpcomingGames } from '../services/api';
+import GameCard from '../components/GameCard';
+
+const UpcomingGames: React.FC = () => {
+    const [games, setGames] = useState<IGame[]>([]);
+
+    useEffect(() => {
+        fetchUpcomingGames().then(setGames);
+    }, []);
+
+    return (
+        <div className="container mt-4">
+            <h1 className="mb-4 text-center">Upcoming Games</h1>
+            <div className="game-grid d-flex flex-wrap gap-4 justify-content-center">
+                {games.map((game) => (
+                    <GameCard key={game.id} game={game} />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default UpcomingGames;

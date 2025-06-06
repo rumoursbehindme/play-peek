c9341d90aa2efd55689964ec627c256c4c5a35e3
Rajeev Hegde <rajeevhegde5@gmail.com>
2025-06-06 17:11:04 +0530

    Make collapse button sticky with arrow icons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { FiSearch } from 'react-icons/fi';
+import { FiSearch, FiChevronUp, FiChevronDown } from 'react-icons/fi';
 import { Navbar as BSNavbar, Nav, Container } from 'react-bootstrap';
 import './styles/NavBar.css';
 import ThemeToggle from './ThemeToggle';
@@ -20,25 +20,19 @@ const Navbar: React.FC = () => {
     };
 
     return (
-        <BSNavbar fixed="top" expand="lg" className={`custom-navbar ${collapsed ? 'navbar-hidden' : ''}` }>
-            <Container>
-                <BSNavbar.Brand as={Link} to="/">PlayPeek</BSNavbar.Brand>
-                <button
-                    className="navbar-collapse-btn ms-2"
-                    onClick={() => setCollapsed((c) => !c)}
-                    aria-label={collapsed ? 'Expand menu' : 'Collapse menu'}
-                >
-                    {collapsed ? 'Expand' : 'Collapse'}
-                </button>
-                <BSNavbar.Toggle aria-controls="basic-navbar-nav" />
-                <BSNavbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto navbar-links">
-                        <Nav.Link as={Link} to="/">Home</Nav.Link>
-                        <Nav.Link as={Link} to="/genres">Genres</Nav.Link>
-                        <Nav.Link as={Link} to="/platforms">Platforms</Nav.Link>
-                        <Nav.Link as={Link} to="/top-rated">Top Rated</Nav.Link>
-                        <Nav.Link as={Link} to="/upcoming">Upcoming</Nav.Link>
-                    </Nav>
+        <div className={`navbar-wrapper ${collapsed ? 'collapsed' : ''}`}> 
+            <BSNavbar expand="lg" className="custom-navbar">
+                <Container>
+                    <BSNavbar.Brand as={Link} to="/">PlayPeek</BSNavbar.Brand>
+                    <BSNavbar.Toggle aria-controls="basic-navbar-nav" />
+                    <BSNavbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto navbar-links">
+                            <Nav.Link as={Link} to="/">Home</Nav.Link>
+                            <Nav.Link as={Link} to="/genres">Genres</Nav.Link>
+                            <Nav.Link as={Link} to="/platforms">Platforms</Nav.Link>
+                            <Nav.Link as={Link} to="/top-rated">Top Rated</Nav.Link>
+                            <Nav.Link as={Link} to="/upcoming">Upcoming</Nav.Link>
+                        </Nav>
 
                     <div className="navbar-tools d-flex align-items-center gap-2 mt-3 mt-lg-0 ms-lg-auto">
                         <form onSubmit={handleSearchSubmit} className="navbar-search-form">
@@ -57,9 +51,17 @@ const Navbar: React.FC = () => {
                         <ThemeToggle />
                     </div>
                 </BSNavbar.Collapse>
-            </Container>
-        </BSNavbar>
+                </Container>
+            </BSNavbar>
+            <button
+                className="navbar-collapse-btn"
+                onClick={() => setCollapsed((c) => !c)}
+                aria-label={collapsed ? 'Expand menu' : 'Collapse menu'}
+            >
+                {collapsed ? <FiChevronDown /> : <FiChevronUp />}
+            </button>
+        </div>
     );
-}; 
+};
 
 export default Navbar;
